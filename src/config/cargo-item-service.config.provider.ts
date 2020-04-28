bf646587ff4cabf5e0ce34a56f601a1cd748948b
abalasu1 <abalasu1@in.ibm.com>
2020-04-28 12:55:43 +0530

    fix test case failures

--- src/config/cargo-item-service.config.provider.ts
+++ src/config/cargo-item-service.config.provider.ts
@@ -1,6 +1,6 @@
 import {ObjectFactory} from 'typescript-ioc';
 
-const baseUrl: string = process.env.SERVICE_URL || 'localhost:8001';
+const baseUrl: string = process.env.CARGOTRACKER-BOOKING-URL || 'localhost:8001';
 
 export const cargoItemConfigFactory: ObjectFactory = () => ({
   baseUrl,
